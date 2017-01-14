e5dbafbbb6de091b97f845a35017b8dee137a934
Oskar Jung <oskar@mbr-targeting.com>
2017-01-14 22:28:02 +0100

    Add tooltip config to doughnut chart

--- public/components/DoughnutChart.jsx
+++ public/components/DoughnutChart.jsx
@@ -3,6 +3,14 @@ import {Pie} from 'react-chartjs-2';
 import {OrderedSet} from 'immutable';
 
 const options = {
+  tooltips: {
+    callbacks: {
+      label: ({index}, {datasets, labels}) => {
+        return labels[index] + ': '
+          + datasets[0].data[index].toLocaleString() + ' Euro';
+      }
+    }
+  }
 };
 
 class DoughnutChart extends React.Component {
